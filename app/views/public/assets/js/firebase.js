b34a2b7438c3d814c5ff82ec5bff54e0942123c8
Troy Crawford <tacrawford91@gmail.com>
2018-03-16 21:36:29 -0500

    added firebase config

---
+++ app/views/public/assets/js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAl03OYcYy8isky1cEiuX_lNto7hiMy1i0",
+    authDomain: "barkpark-a39cc.firebaseapp.com",
+    databaseURL: "https://barkpark-a39cc.firebaseio.com",
+    projectId: "barkpark-a39cc",
+    storageBucket: "barkpark-a39cc.appspot.com",
+    messagingSenderId: "1026988096884"
+  };
+  firebase.initializeApp(config);
