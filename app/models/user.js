49f86163306583d2a1c0c5e3a9be532ea65c8a20
Troy Crawford <tacrawford91@gmail.com>
2018-03-17 11:38:32 -0500

    User Routes and park routes completed

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-      uuid: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV1,
-        primaryKey: true
-      },
       user_name: DataTypes.STRING,
       password: DataTypes.STRING,
       email: DataTypes.STRING,
