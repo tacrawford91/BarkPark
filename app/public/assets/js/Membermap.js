67fbd392ddc0b04cf959c7736b3394c2bcea304c
eddiezhaor <eddiezhaor@gmail.com>
2018-03-23 18:21:37 -0500

    membermap.js

--- app/public/assets/js/Membermap.js
+++ app/public/assets/js/Membermap.js
@@ -42,16 +42,6 @@ function initMap() {
 
 }
 
-
-// var newMarker2 = [];
-// var lat = [];
-// var long = [];
-// var parkName = [];
-// var address = [];
-// var mylat;
-
-
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -91,32 +81,10 @@ function addMarker(data) {
             icon: icon
 
         })
-
-
         marker.set("id", 1);
         marker.setPosition(cnt);
         myMarker.push(marker);
         var infowindow = new google.maps.InfoWindow();
-        // console.log(data[i].park_name)
-        // var searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
-        // google.maps.event.addListener(searchBox, "places_changed", function() {
-        //     // removeMarkers()
-        //     var marker2 = new google.maps.Marker({
-        //         map: map,
-        //         icon: icon
-        //     });
-        //     var places = searchBox.getPlaces()
-        //     var bounds = new google.maps.LatLngBounds();
-        //     bounds.extend(places[0].geometry.location);
-        //     marker2.setPosition(places[0].geometry.location);
-        //     google.maps.event.addListener(marker2, 'click', function() {
-        //         infowindow.setContent(places[0].name);
-        //         infowindow.open(map, this);
-        //     });
-        //     map.fitBounds(bounds);
-        //     map.setZoom(10)
-        // })
-
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 // $.post("/api/park"+data[i].id,{},function(res){
@@ -131,18 +99,6 @@ function addMarker(data) {
                     $(".secondFooter").empty()
                     $(".secondFooter").append(`<p class="text-center"><button data-id=${i} type="button" id="submitData" class="btn btn-default" data-dismiss="modal">Submit</button></p>
                         `)
-                        // result.forEach(element => {
-
-                    //     var userDiv = $("<div>").addClass("userDiv");                        // $("#userInfor").append(JSON.stringify(element["dog_name"]).replace(/\"/g, "") + "<br>")("userDiv");
-                    //     var userImg = $("<img>").attr("src",element.picture).addClass("userImg");
-                    //     var userName= $("<h4>").addClass("userName").text(element.dog_name);
-                    //     var userBreed = $("<h4>").addClass("userBreed").text(element.breed);
-                    //     var userOwner = $("<h5>").addClass("userOwner").text(`Owner: ${element.owner_Name}`);
-                    //     var userThumbsUp = $("<h4>").addClass("userThumbsUp").html(`<i class="fa fa-thumbs-up" aria-hidden="true"></i>: ${element.thumbsUp}`);
-                    //     var userThumbsDown = $("<h4>").addClass("userThumbsDown").html(`<i class="fa fa-thumbs-down" aria-hidden="true"></i>: ${element.thumbsUp}`);
-                    //     userDiv.append(userImg,userName,userBreed, userOwner, userThumbsUp,userThumbsDown);
-                    //     $("#userInfor").append(userDiv);
-                    // });
                     $(".firstFooter").empty()
                     $(".firstFooter").append(`<button data-id=${i} id="checkin" type="button" class=" btn btn-default">Check In</button>`).append(` <button type="button" data-id=${i} id="seeUsers" class="btn btn-default" data-dismiss="modal">See who is here!</button>`)
                     $('#myModal').modal('show');
@@ -181,10 +137,6 @@ function addMarker(data) {
             console.log(c)
             $.get("/api/user/park/" + c, function(myData) {
                 $("#userInfor").empty()
-                    // var image = new Image();
-                    // console.log(myData[0]['picture'])
-                    // image.src = myData[0]["picture"];
-                    // $("#userInfor").append(image)
                 myData.forEach(element => {
                     var userDiv = $("<div>").addClass("userDiv");
                     var userImg = $("<img>").attr("src", element.picture).addClass("userImg");
@@ -216,10 +168,6 @@ function addMarker(data) {
                     $.get("/api/user/park/" + rightID, function(myData) {
                         $("#userInfor").empty()
                         $("#myModal").modal("hide")
-                            // var image = new Image();
-                            // console.log(myData[0]['picture'])
-                            // image.src = myData[0]["picture"];
-                            // $("#userInfor").append(image)
                         myData.forEach(element => {
                             var userDiv = $("<div>").addClass(`userDiv userDiv${element.id}`); // $("#userInfor").append(JSON.stringify(element["dog_name"]).replace(/\"/g, "") + "<br>")("userDiv");
                             var userImg = $("<img>").attr("src", element.picture).addClass("userImg");
@@ -237,27 +185,17 @@ function addMarker(data) {
                 })
 
             })
-
-
             console.log(dogCountUpdate)
-
-
             $.ajax({
-                url: `/api/park/newDog/${dogCountUpdate.parkID}`,
-                method: "PUT",
-                data: dogCountUpdate
-            }).then((data) => {
-                console.log(`updated park id${dogCountUpdate.parkID} with ${dogCountUpdate.dog_count}`);
-                socket.emit("dogCountUpdate", { dogCountUpdate });
-
-            })
-
-
-            // setNumber(marker)
-
-
+                    url: `/api/park/newDog/${dogCountUpdate.parkID}`,
+                    method: "PUT",
+                    data: dogCountUpdate
+                }).then((data) => {
+                    console.log(`updated park id${dogCountUpdate.parkID} with ${dogCountUpdate.dog_count}`);
+                    socket.emit("dogCountUpdate", { dogCountUpdate });
+                })
+                // setNumber(marker)
         });
-
         //Update thumbs up when given one
         $(document).on("click", ".plusOne", function() {
             // $(this).hide();
@@ -286,12 +224,7 @@ function addMarker(data) {
         // update thumbs down when given ones
         $(document).on("click", ".downOne", function() {
             var userID = $(this).attr("data-id");
-            // $(this).hide();
-            // $(".plusOne").hide();
             $(`.hide${userID}`).hide();
-            // $(`.userDiv${userID}`).append($("<h5>").text("Vote Added!"));
-            // var c = Number(btnID) + 1
-            // console.log(c)
             $.get(`/api/user/thumbs/${userID}`, function(myData) {
                 console.log(myData);
                 var updateThumbsDown = Number(myData.thumbsDown) + 1;
@@ -322,7 +255,6 @@ function addMarker(data) {
         socket.on("dogCountUpdate", function(data) {
             console.log(data.data.dogCountUpdate.marker);
             var mymark = myMarker[data.data.dogCountUpdate.marker]
-                // console.log(mymark)
             $.ajax({
                 url: `/api/park/${data.data.dogCountUpdate.parkID}`,
                 method: "GET"
@@ -331,12 +263,7 @@ function addMarker(data) {
                     color: "red",
                     text: data.dog_count.toString()
                 }
-
-
                 mymark.setLabel(newLabel)
-                    // $("#dogNumber").val("")
-
-
             })
         })
     }
@@ -345,144 +272,6 @@ function addMarker(data) {
 function setNumber(marker) {
     marker.setLabel()
 }
-// function placeMarker(location) {
-
-// }
-// var marker = new google.maps.Marker({
-//     position: location, 
-//     map: map
-// });
-
-// var icon = {
-//     url: "../image/dog.png",
-//     labelOrigin: new google.maps.Point(9, -8),
-//     scaledSize: new google.maps.Size(20, 20),
-//     origin: new google.maps.Point(0, 0), // origin
-// }
-
-// function initMap() {
-
-//     console.log(mylat)
-//     var chicago = {
-//         lat: 41.8781,
-//         lng: -87.6298
-//     };
-
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: chicago,
-//         zoom: 11
-//     });
-//     var autocomplete = new google.maps.places.Autocomplete(
-//         /** @type {!HTMLInputElement} */
-//         (
-//             document.getElementById('pac-input')));
-//     $("#pac-input").keyup(function(e) {
-//         if (e.KeyCode = 13) {
-//             if ($("#pac-input").val() == "") {
-//                 addMarkers();
-//                 removeMarker2();
-//             }
-//         }
-//     })
-// var searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
-//     google.maps.event.addListener(searchBox, "places_changed", function() {
-//         removeMarkers()
-//         var marker2 = new google.maps.Marker({
-//             map: map,
-//             icon: icon
-//         });
-//         var places = searchBox.getPlaces()
-//         var bounds = new google.maps.LatLngBounds();
-//         bounds.extend(places[0].geometry.location);
-//         marker2.setPosition(places[0].geometry.location);
-//         google.maps.event.addListener(marker2, 'click', function() {
-//             infowindow.setContent(places[0].name);
-//             infowindow.open(map, this);
-//         });
-//         map.fitBounds(bounds);
-//         map.setZoom(10)
-//         newMarker2.push(marker2);
-//         for (var j = 0; j < newMarker2.length; j++) {
-//             if (j === 0) {
-//                 newMarker2[0].setVisible(true)
-//             } else {
-//                 newMarker2[j].setVisible(true)
-//                 newMarker2[j - 1].setVisible(false)
-//             }
-//         }
-//     });
-//     infowindow = new google.maps.InfoWindow();
-//     var service = new google.maps.places.PlacesService(map);
-//     service.nearbySearch({
-//         location: chicago,
-//         radius: 100000,
-//         keyword: "dog park"
-//     }, callback);
-// }
-
-// function callback(results, status) {
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//             results[i].number = i;
-//             createMarker(results[i]);
-//         }
-//         // $.ajax({
-//         // console.log(myMarker)
-//         // $.post("/api/park", { "address": address }, function(data) {
-//         //         console.log("nice")
-//         //     })
-//         // })
-//     }
-// }
-
-// function createMarker(place) {
-//     console.log(place)
-//     var placeLoc = place.geometry.location;
-//     var icon = {
-//         url: "./dog.png",
-//         labelOrigin: new google.maps.Point(9, -8),
-//         scaledSize: new google.maps.Size(20, 20),
-//         origin: new google.maps.Point(0, 0), // origin
-//         anchor: new google.maps.Point(11, 40)
-//     }
-//     var marker = new google.maps.Marker({
-//         map: map,
-//         position: placeLoc,
-//         label: {
-//             color: "red",
-//             text: place.number.toString()
-//         },
-//         icon: icon,
-//     });
-//     myMarker.push(marker);
-//     lat.push(marker.position.lat())
-//     long.push(marker.position.lng())
-//     parkName.push(place.name)
-//     address.push(place.vicinity)
-//     google.maps.event.addListener(marker, 'click', function() {
-//         infowindow.setContent(place.name);
-//         infowindow.open(map, this);
-//     });
-// }
-
-// function removeMarkers() {
-//     for (var i = 0; i < myMarker.length; i++) {
-//         myMarker[i].setVisible(false);
-//     }
-// }
-
-// function addMarkers() {
-//     for (var k = 0; k < myMarker.length; k++) {
-//         myMarker[k].setVisible(true);
-//     }
-// }
-
-// function removeMarker2() {
-//     for (var l = 0; l < newMarker2.length; l++) {
-//         newMarker2[l].setVisible(false);
-//     }
-// }
-
 
 
 $("#addClass").click(function() {
@@ -498,14 +287,4 @@ $(document).on("click", "#checkin", function() {
     var input = $(`<input type="checkbox" name="list" value="house">house<br>`)
     $("#checkinForm").append(input)
     $("#checkinModal").modal("show")
-
 })
-
-
-
-//Magic Below 
-
-
-// $("#submit").click(function() {
-
-// })
